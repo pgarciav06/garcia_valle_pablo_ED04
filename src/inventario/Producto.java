160676b2d08593879304f75b8e6e060653a6325b
Pablo García Valle <>
2025-03-07 00:13:25 +0100

    Encapsulados los atributos de Productos

--- src/inventario/Producto.java
+++ src/inventario/Producto.java
@@ -4,6 +4,30 @@ public class Producto {
     public String n;
     public double p;
     public int s;
+    
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
+
+    public double getP() {
+        return p;
+    }
+
+    public void setP(double p) {
+        this.p = p;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public void setS(int s) {
+        this.s = s;
+    }
 
     public Producto(String nombre, double precio, int stock) {
         this.n = nombre;
